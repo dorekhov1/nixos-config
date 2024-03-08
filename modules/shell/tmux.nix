c68acb9d425d776619382943e5529c09c34fa3ab
Daniil Orekhov <daniil.orekhov15@gmail.com>
2024-03-08 22:55:48 +0300

    some tmux configuration (will probably abandon in favor of zellij)

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -29,30 +29,6 @@
           extrakto
           better-mouse-mode
           {
-            plugin = catppuccin;
-            extraConfig = ''
-              set -g @catppuccin_flavour 'mocha'
-              set -g @catppuccin_window_left_separator ""
-              set -g @catppuccin_window_right_separator " "
-              set -g @catppuccin_window_middle_separator " █"
-              set -g @catppuccin_window_number_position "right"
-              set -g @catppuccin_window_default_fill "number"
-              set -g @catppuccin_window_default_text "#W"
-              set -g @catppuccin_window_current_fill "number"
-              set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-              set -g @catppuccin_status_modules_right "directory meetings date_time"
-              set -g @catppuccin_status_modules_left "session"
-              set -g @catppuccin_status_left_separator  " "
-              set -g @catppuccin_status_right_separator " "
-              set -g @catppuccin_status_right_separator_inverse "no"
-              set -g @catppuccin_status_fill "icon"
-              set -g @catppuccin_status_connect_separator "no"
-              set -g @catppuccin_directory_text "#{b:pane_current_path}"
-              set -g @catppuccin_meetings_text "#($HOME/.config/tmux/scripts/cal.sh)"
-              set -g @catppuccin_date_time_text "%H:%M"
-            '';
-          }
-          {
             plugin = inputs.tmux-sessionx.packages."${pkgs.system}".default;
             extraConfig = ''
               set -g @sessionx-x-path '~/.dotfiles'
@@ -71,9 +47,26 @@
           {
             plugin = continuum;
             extraConfig = ''
-              set -g @continuum-save-interval '5'
+              set -g @continuum-save-interval '15'
               set -g @continuum-restore 'on'
             '';
+          }
+          {
+            plugin = catppuccin;
+            extraConfig = ''
+              set -g @catppuccin_flavour 'mocha'
+              set -g @catppuccin_window_left_separator "█"
+              set -g @catppuccin_window_right_separator "█ "
+              set -g @catppuccin_window_number_position "right"
+              set -g @catppuccin_window_middle_separator "  █"
+              set -g @catppuccin_status_left_separator  "█"
+              set -g @catppuccin_status_right_separator "█ "
+            '';
+              # set -g @catppuccin_status_right_separator_inverse "no"
+              # set -g @catppuccin_status_fill "icon"
+              # set -g @catppuccin_status_modules_left "session"
+              # set -g @catppuccin_directory_text "#{b:pane_current_path}"
+              # set -g @catppuccin_date_time_text "%H:%M"
           }
         ];
         extraConfig = ''
