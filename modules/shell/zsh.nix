44672f5b64b4cb50472be96bedd2ed0bbdb68bbd
Daniil Orekhov <daniil.orekhov15@gmail.com>
2024-03-20 19:29:53 +0300

    cli vpn toggle function

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -14,7 +14,44 @@
         dotDir = ".config/zsh";
   
         initExtra = ''
-          export http_proxy=http://127.0.0.1:2334;export https_proxy=http://127.0.0.1:2334;export ALL_PROXY=socks5://127.0.0.1:2334
+          proxy_http="http://127.0.0.1:2334"
+          proxy_socks="socks5://127.0.0.1:2334"
+
+          set_proxy() {
+            export http_proxy="$proxy_http"
+            export https_proxy="$proxy_http"
+            export ALL_PROXY="$proxy_socks"
+          }
+
+          unset_proxy() {
+            unset http_proxy
+            unset https_proxy
+            unset ALL_PROXY
+          }
+
+          toggle_proxy() {
+            if [[ -n "$http_proxy" ]]; then
+              unset_proxy
+              echo "Proxy settings have been disabled."
+            else
+              set_proxy
+              echo "Proxy settings have been enabled."
+            fi
+          }
+
+          check_proxy() {
+            if [[ -n "$http_proxy" ]]; then
+              echo "Proxy is currently set to:"
+              echo "HTTP_PROXY: $http_proxy"
+              echo "HTTPS_PROXY: $https_proxy"
+              echo "ALL_PROXY: $ALL_PROXY"
+            else
+              echo "Proxy is currently disabled."
+            fi
+          }
+
+          set_proxy
+
           if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
             exec tmux attach
           fi
