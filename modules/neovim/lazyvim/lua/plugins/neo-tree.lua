ab4fdd23d0a84c2356bdbf84c6453452c3bf57ee
Daniil Orekhov <daniil.orekhov15@gmail.com>
2024-03-20 19:29:00 +0300

    neotree setup

--- modules/neovim/lazyvim/lua/plugins/neo-tree.lua
+++ modules/neovim/lazyvim/lua/plugins/neo-tree.lua
@@ -2,8 +2,28 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      hijack_netrw_behavior = "open_default",
       filtered_items = {
         visible = true,
+        never_show = {
+          ".git",
+          ".direnv",
+          ".mypy_cache",
+          ".pyre",
+          ".ruff_cache",
+          "result",
+        },
+        never_show_by_pattern = {
+          "**/__pycache__",
+        },
+      },
+    },
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.cmd([[setlocal relativenumber]])
+        end,
       },
     },
   },
