af60cbecf24bb75a0050d4b9020b670cca040bdd
Daniil Orekhov <daniil.orekhov15@gmail.com>
2024-03-27 11:16:58 +0300

    custom python config

--- modules/neovim/lazyvim/lua/plugins/python.lua
+++ modules/neovim/lazyvim/lua/plugins/python.lua
@@ -70,6 +70,14 @@ return {
       },
       config = function()
         require("dap-python").setup("python")
+        table.insert(require("dap").configurations.python, {
+          type = "python",
+          request = "launch",
+          name = "My custom launch configuration",
+          program = "${file}",
+          justMyCode = false,
+          -- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+        })
       end,
     },
   },
