587aee11575656354d9e42dbf81a5daaaccffc81
Daniil Orekhov <daniil.orekhov15@gmail.com>
2025-12-10 17:19:33 +0300

    fix: zsh config

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
 
@@ -25,50 +25,51 @@
           }
         ]; 
 
-        initContent = ''
-          proxy_http="http://127.0.0.1:2334"
-          proxy_socks="socks5://127.0.0.1:2334"
+        initContent =
+          let
+            enableProxy = (config.networking.hostName or "") != "vps";
+          in lib.optionalString enableProxy ''
+            proxy_http="http://127.0.0.1:2334"
+            proxy_socks="socks5://127.0.0.1:2334"
 
-          set_proxy() {
-            export http_proxy="$proxy_http"
-            export https_proxy="$proxy_http"
-            export ALL_PROXY="$proxy_socks"
-            export no_proxy="$no_proxy_addr"
-          }
+            set_proxy() {
+              export http_proxy="$proxy_http"
+              export https_proxy="$proxy_http"
+              export ALL_PROXY="$proxy_socks"
+              export no_proxy="$no_proxy_addr"
+            }
 
-          unset_proxy() {
-            unset http_proxy
-            unset https_proxy
-            unset ALL_PROXY
-            unset no_proxy
-          }
+            unset_proxy() {
+              unset http_proxy
+              unset https_proxy
+              unset ALL_PROXY
+              unset no_proxy
+            }
 
-          toggle_proxy() {
-            if [[ -n "$http_proxy" ]]; then
-              unset_proxy
-              echo "Proxy settings have been disabled."
-            else
-              set_proxy
-              echo "Proxy settings have been enabled."
-            fi
-          }
+            toggle_proxy() {
+              if [[ -n "$http_proxy" ]]; then
+                unset_proxy
+                echo "Proxy settings have been disabled."
+              else
+                set_proxy
+                echo "Proxy settings have been enabled."
+              fi
+            }
 
-          check_proxy() {
-            if [[ -n "$http_proxy" ]]; then
-              echo "Proxy is currently set to:"
-              echo "HTTP_PROXY: $http_proxy"
-              echo "HTTPS_PROXY: $https_proxy"
-              echo "ALL_PROXY: $ALL_PROXY"
-              echo "NO_PROXY: $no_proxy"
-            else
-              echo "Proxy is currently disabled."
-            fi
-          }
+            check_proxy() {
+              if [[ -n "$http_proxy" ]]; then
+                echo "Proxy is currently set to:"
+                echo "HTTP_PROXY: $http_proxy"
+                echo "HTTPS_PROXY: $https_proxy"
+                echo "ALL_PROXY: $ALL_PROXY"
+                echo "NO_PROXY: $no_proxy"
+              else
+                echo "Proxy is currently disabled."
+              fi
+            }
 
-          set_proxy
-
-          export ANTHROPIC_API_KEY="$(cat ${config.sops.secrets.anthropic-api-key.path})"
-        '';
+            set_proxy
+          '';
       };
     };
   };
